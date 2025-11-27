fc447026f6a40c12c84fdaec198459e3fc39cf08
MrHsource <mrhelleeee@gmail.com>
2025-11-27 13:03:49 +0100

    R&M

---
+++ script.js
@@ -0,0 +1,6 @@
+// Select the booking form
+const form = document.getElementById("bookingForm");
+
+//only resets when coming back from fromsprees thank you page
+window.addEventListener("pageshow", (event) => {
+    const if 
